1eb77fd6f8b266e984871a92ed0474199b811880
DobbaWon <charlie.j.dobson@outlook.com>
2025-11-06 22:42:09 +0000

    Changed YAML

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",       // enables static HTML export
+  distDir: "out",         // optional: folder for built files
 };
 
-export default nextConfig;
+module.exports = nextConfig;
