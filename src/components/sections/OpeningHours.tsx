8c594c21e193b68c48f48c7717ca951cfce96add
Charles Dobson <79115124+DobbaWon@users.noreply.github.com>
2025-11-07 11:27:56 +0000

    Update contact link to Google Maps

--- src/components/sections/OpeningHours.tsx
+++ src/components/sections/OpeningHours.tsx
@@ -153,7 +153,7 @@ export default function OpeningHours() {
         </div>
       </div>
         <div className={styles.contact}>
-          <a href="https://goo.gl/maps/your-location" target="_blank" rel="noopener noreferrer">📍169 St Paul's Rd, Deepdale, Preston PR1 1PX</a>
+          <a href="https://www.google.com/maps/place/Chaii+Stop/@53.7676435,-2.6989896,17z/data=!3m1!4b1!4m6!3m5!1s0x487b733a6e4fc43b:0x538645d28f41e0d0!8m2!3d53.7676435!4d-2.6964147!16s%2Fg%2F11q3m97892?entry=ttu&g_ep=EgoyMDI1MTEwNS4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer">📍169 St Paul's Rd, Deepdale, Preston PR1 1PX</a>
           <a href='tel:01772846558' className={styles.phone}>📞01772 846558</a>
         </div>
       </div>
