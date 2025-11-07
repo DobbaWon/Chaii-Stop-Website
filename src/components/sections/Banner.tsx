89bad7f5d6c48e2b260263b187724a4a34105f3d
DobbaWon <charlie.j.dobson@outlook.com>
2025-11-07 09:58:41 +0000

    added mobile new banner

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -4,6 +4,7 @@ export default function Banner() {
   return (
     <div className={styles.banner}>
       <img src="/images/banner.jpg" alt="banner" className={styles.bannerImg} />
+      <img src="/images/bannerMobile.png" alt="banner" className={styles.mobileBannerImg} />
     </div>
   );
 }
